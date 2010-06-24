e9907e259061b92f5bd199344c085fb60314e983
func09 <mitsuru.haga@gmail.com>
2010-06-25 02:43:42 +0900

    ステータス保存時にツイートする機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < TwitterAuth::GenericUser
   has_many :statuses
+  
+  class << self
+    def app_user
+      @app_user if defined? @app_user
+      @app_user = find_by_login(APP_CONFIG[:twitter]['app_user'])
+    end
+  end
+  
   # Extend and define your user model as you see fit.
   # All of the authentication logic is handled by the 
   # parent TwitterAuth::GenericUser class.
