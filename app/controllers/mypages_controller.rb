b083214401d717cd4be331ae2e99b3a5c49d576d
func09 <mitsuru.haga@gmail.com>
2010-06-29 16:47:23 +0900

    マイページのリストを修正

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,8 +2,10 @@ class MypagesController < ApplicationController
   
   before_filter :login_required
   
+  @@per_page = 20
+  
   def show
-    @statuses = current_user.statuses
+    @statuses = current_user.statuses.paginate(:page => params[:page], :per_page => @@per_page, :order => 'created_at DESC')
   end
 
 end
