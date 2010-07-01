9411a9800dd323662e6089a3c97d0b4dd0f277f3
func09 <mitsuru.haga@gmail.com>
2010-07-01 18:14:56 +0900

    CRONで名無しアカウントの情報を更新するようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < TwitterAuth::GenericUser
       return @app_user if defined? @app_user
       @app_user = find_by_login(APP_CONFIG[:twitter]['app_user'])
     end
+    def update_app_user
+      u = User.identify_or_create_from_access_token(app_user.access_token, app_user.access_secret)
+      if u
+        app_user.reload
+        true
+      else
+        false
+      end
+    end
   end
   
   # Extend and define your user model as you see fit.
