b9525b5f89b642e575a9582fc91d7fe22dff63ee
func09 <mitsuru.haga@gmail.com>
2010-08-09 22:09:39 +0900

    環境変数読みコミの処理修正

--- config/initializers/00_load_env.rb
+++ config/initializers/00_load_env.rb
@@ -1,10 +1,12 @@
 # Load ENV
-
-[File.expand_path('env.rb',Rails.root),
-  File.expand_path('../../shared/env.rb', Rails.root)].each do |env_file|
-  require env_file
-  p "load env file #{env_file}"
-  break
+# 1. Try load ./env.rb
+# 2. Try load ../../shared/env.rb
+[File.expand_path('env.rb',Rails.root), File.expand_path('../../shared/env.rb', Rails.root)].each do |env_file|
+  if File.exists? env_file
+    require env_file
+    p "load env file #{env_file}"
+    break
+  end
 end
 
 
